a728ff371359603fd374b0ef26770e27e89b0fbd
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-08-19 00:45:37 +0700

    PUT PATCH DELETE

---
+++ 2.1-RestAssured_PUT_PATCH_DELETE/src/test/java/tests/Put.java
@@ -0,0 +1,33 @@
+package tests;
+
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class Put {
+
+	@Test
+	public void get() {
+		baseURI = "https://reqres.in/api";
+		// API POST "https://reqres.in/api/users"
+
+		JSONObject json = new JSONObject();
+
+		json.put("name", "morpheus");
+		json.put("job", "leader");
+
+		System.out.println(json.toJSONString());
+
+		given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON)
+				.body(json.toJSONString()).when().put("/users/2").then().statusCode(200).log().all();
+
+	}
+
+}
